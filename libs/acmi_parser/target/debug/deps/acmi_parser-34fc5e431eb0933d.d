15ead420b2f145b251a697e3684e76b849a3f022
Carl-Erik Bergström <calle.bergstrom@live.se>
2022-07-05 21:01:29 +0200

    Initialise NX with web and acmi_parser modules

---
+++ libs/acmi_parser/target/debug/deps/acmi_parser-34fc5e431eb0933d.d
@@ -0,0 +1,5 @@
+/home/callebstrom/dev/retac/libs/acmi_parser/target/debug/deps/acmi_parser-34fc5e431eb0933d.rmeta: src/main.rs
+
+/home/callebstrom/dev/retac/libs/acmi_parser/target/debug/deps/acmi_parser-34fc5e431eb0933d.d: src/main.rs
+
+src/main.rs:
